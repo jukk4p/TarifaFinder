f75229d30ef657a97fd289c051d48c55d27e23c6
Iván González Ufano <jukk4p@gmail.com>
2025-07-10 10:25:17 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="dark">
+    <html>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
